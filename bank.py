fe1d668e414241f31dccdc0253e3e2626db704da
sumeetby16 <sumeetby16>
2025-10-23 16:15:10 +0530

    bank acc

--- bank.py
+++ bank.py
@@ -5,3 +5,16 @@ deposit_amount = float(input("Enter deposit amount: ₹"))
 new_balance = initial_balance + deposit_amount
 
 print(f"New Balance after deposit: ₹{new_balance}")
+
+
+
+initial_balance = float(input("Enter initial balance: ₹"))
+deposit_amount = float(input("Enter deposit amount: ₹"))
+
+new_balance = initial_balance + deposit_amount
+print(f"New Balance after deposit: ₹{new_balance}")
+
+withdraw_amount = float(input("Enter withdrawal amount: ₹"))
+
+final_balance = new_balance - withdraw_amount
+print(f"Final Balance: ₹{final_balance}")
