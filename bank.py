93d82b5f057b9a3500ed39526f1038ca6c7ee7f3
sumeetby16 <sumeetby16>
2025-10-23 16:11:42 +0530

    bank acc

---
+++ bank.py
@@ -0,0 +1,7 @@
+
+initial_balance = float(input("Enter initial balance: ₹"))
+deposit_amount = float(input("Enter deposit amount: ₹"))
+
+new_balance = initial_balance + deposit_amount
+
+print(f"New Balance after deposit: ₹{new_balance}")
